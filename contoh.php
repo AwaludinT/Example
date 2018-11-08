f915b13c433d9837a832dd9b843a08032509330c
AwaludinT <43643847+AwaludinT@users.noreply.github.com>
2018-11-09 02:19:52 +0700

    Add files via upload

---
+++ contoh.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+	</title>
+</head>
+<body>
+<?php
+	echo "4 + 5 = ",4+5;//menampilkan hasil 4=5
+	//this is a comment
+	/*this is
+	a comment
+	block*/
+?>
+
+</body>
+</html>
